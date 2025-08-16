e7ccf2fe8e537316e45079c31758942f0c08d369
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-16 20:11:55 +0200

    Create Frynmannmotivestononquantumregisters.h

---
+++ Release_one_dripped/Frynmannmotivestononquantumregisters.h
@@ -0,0 +1,4 @@
+//the namenof the fioe is right. in order to keep track
+//of the datas, we must rely on chips first then we have to worry
+//about cpu registers. as the chips are not described in the readme,
+//we have to prevent using models 
